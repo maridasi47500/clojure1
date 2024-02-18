c39f0e1745623f1e0d77a3f23d33bac271137a3e
Pirata Del Barco <anonyme201504@gmail.com>
2024-02-18 19:08:39 -0300

    Hey

--- project.clj
+++ project.clj
@@ -7,6 +7,12 @@
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [javax.xml.bind/jaxb-api "2.3.1"]
                  [http-kit "2.2.0"]
-                 [clj-time "0.14.0"]]
+                 [clj-time "0.14.0"]
+                 [org.clojure/java.jdbc "0.7.12"]
+                 [metosin/jsonista "0.3.8"]
+                 [org.xerial/sqlite-jdbc "3.15.1"]
+                 [cheshire "5.12.0"]
+                 [ring/ring-codec "1.2.0"]
+]
   :repl-options {:init-ns clojurenew.core}
   :main clojurenew.core)
