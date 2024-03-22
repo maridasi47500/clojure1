6d923c123fc212c89e6ca23576c33b4ea5aedd37
nave pirata <anonyme201504@gmail.com>
2024-03-21 21:42:21 -0300

    update

--- src/clojurenew/core.clj
+++ src/clojurenew/core.clj
@@ -42,11 +42,44 @@
   "execute query and return lazy sequence"
   []
   (query db ["select * from bath"]))
+(defn getbyid
+  "execute query and return lazy sequence"
+  [myid]
+  (query db ["select * from bath where id = ?" myid]))
 
+(defn voirbain [title template req myid]
+  (println "action voirbain")
+  (println req)
+  (def mytemplate (slurp (io/resource template)))
+  (def figure (map #(format mytemplate (get % :title "") (get % :body "") (get % :id "")) (getbyid myid)))
+  (def body (format (slurp (io/resource "bains.html")) (str/join "" figure)))
+  (def title "hey")
+  (def hey (if (zero? (count (output))) "<p>il n'y a pas de bain à afficher</p>" body))
+  (def reponsebody (format (slurp (io/resource "index.html")) title hey))
+  {:status 200
+    :body reponsebody
+    :contenttype "text/html"
+    :redirect ""
+    })
+(defn editbain [title template req myid]
+  (println "action voirbain")
+  (def mytemplate (slurp (io/resource template)))
+  (println "action EDIT bain")
+  (println (getbyid myid))
+  (def figure (map #(format mytemplate (get % :id "") (get % :title "") (get % :body "") (get % :url "") (get % :id "")) (getbyid myid)))
+  (def body (format (slurp (io/resource "bains.html")) (str/join "" figure)))
+  (def title "hey")
+  (def hey (if (zero? (count (output))) "<p>il n'y a pas de bain à afficher</p>" body))
+  (def reponsebody (format (slurp (io/resource "index.html")) title hey))
+  {:status 200
+    :body reponsebody
+    :contenttype "text/html"
+    :redirect ""
+    })
 (defn rendercollection [title bdd template req]
   (println "action create")
   (def mytemplate (slurp (io/resource template)))
-  (def figure (map #(format mytemplate (get % :url "") (get % :title "") (get % :body "")) (output)))
+  (def figure (map #(format mytemplate  (get % :title "") (get % :body "") (get % :id "") (get % :url "") (get % :id "")) (output)))
   (def body (format (slurp (io/resource "bains.html")) (str/join "" figure)))
   (def title "hey")
   (def hey (if (zero? (count (output))) "<p>il n'y a pas de bains à afficher</p>" body))
@@ -55,6 +88,31 @@
     :body reponsebody
     :contenttype "text/html"
     :redirect ""
+    })
+(defn actionupdate [title hey req]
+  (println "action create")
+  (def heyreader (io/reader (:body req) :encoding "UTF-8"))
+  (println heyreader)
+  (println "hey HEY action create")
+  (println "hHo")
+  (def a (slurp heyreader) )
+  (println a)
+
+  (def myhash (keywordize-keys (form-decode a)))
+  (println "hey HEY he")
+  (println myhash)
+  (def mytitle (get myhash :title "hye"))
+  (def url (get myhash :url "url"))
+  (def body (str/join "" (get myhash :body ["ur" "body"]) ))
+  (def id (get myhash :id ""))
+  (execute! db
+  ["update bath set title = ?, url = ?, body = ? where id = ?"
+   mytitle url body id])
+  (println "OHOHOH")
+  {:status 301
+    :body (slurp (io/resource "redirect.html"))
+    :contenttype "text/html"
+    :redirect "/voir_bains"
     })
 
 (defn actioncreate [title hey req]
@@ -85,7 +143,8 @@
   []
   (try (db-do-commands db
                        (create-table-ddl :bath
-                                         [[:timestamp :datetime :default :current_timestamp ]
+                                         [[:id "integer primary key autoincrement" ]
+                                          [:timestamp :datetime :default :current_timestamp ]
                                           [:url :text]
                                           [:title :text]
                                           [:body :text]]))
@@ -112,18 +171,31 @@
   (println "bdd")
   (println (output))
   (println "my application")
-  (println req)
+  #(println req)
   (println (get req :uri "aucune adresse url"))
   (def uri (get req :uri "aucune adresse url"))
+  (def hey (let [mystr uri]
+    (cond
+          (re-find #"^/$" uri) "welcome.html"
+          (re-find #"^/hello$" uri) "hello.html"
+          (re-find #"^/create_bath$" uri) "uhiuh"
+          (re-find #"^/action_create_bath$" uri) "lk"
+          (re-find #"^/voir_bains$" uri) "lilu"
+          (re-find #"^/voir_bain/\d+$" uri) "i"
+          (re-find #"^/edit_bain/\d+$" uri) "i"
+          :else "404.html")))
+  (println hey)
   (def html (let [mystr uri]
-    (case mystr
-          "/" (renderhtml "hey" "welcome.html")
-          "/hello" (renderhtml "hello" "hello.html")
-          "/create_bath" (renderhtml "hello" "form.html")
-          "/action_create_bath" (actioncreate "hello" "form.html" req)
-          "/voir_bains" (rendercollection "hello" output "_bain.html" req)
-
-          (renderhtml "Erreur" "404.html"))))
+    (cond
+          (re-find #"^/$" uri) (renderhtml "hey" "welcome.html")
+          (re-find #"^/hello$" uri) (renderhtml "hello" "hello.html")
+          (re-find #"^/create_bath$" uri) (renderhtml "hello" "form.html")
+          (re-find #"^/action_create_bath$" uri) (actioncreate "hello" "form.html" req)
+          (re-find #"^/action_update_bath$" uri) (actionupdate "hello" "formedit.html" req)
+          (re-find #"^/voir_bains$" uri) (rendercollection "hello" output "_bain.html" req)
+          (re-find #"^/voir_bain/\d+$" uri) (voirbain "hello" "voirbain.html" req (get (re-find #"^/voir_bain/(\d+)$" uri) 1))
+          (re-find #"^/edit_bain/\d+$" uri) (editbain "hello" "formedit.html" req (get (re-find #"^/edit_bain/(\d+)$" uri) 1))
+          :else (renderhtml "Erreur" "404.html"))))
   (def status (get html :status 200))
   (def content (get html :contenttype "text/html"))
   (def body (get html :body "<h1>erreur 404 desole</h1>"))
